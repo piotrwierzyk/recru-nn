29551795aea5e58180a1ce6170b4b6221a970b73
Piotr Wierzyk <piwierzyk@gmail.com>
2025-10-24 09:54:04 +0200

    quick fix

--- azure/bootstrap/main.tf
+++ azure/bootstrap/main.tf
@@ -42,7 +42,7 @@ resource "azurerm_storage_account" "terraform_state" {
 
 resource "azurerm_storage_container" "terraform_state" {
   name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.terraform_state.name
+  storage_account_id    = azurerm_storage_account.terraform_state.id
   container_access_type = "private"
 }
 
